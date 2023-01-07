54447df0f3734ff7e518f9fa0adf1f14a09574f8
gevasoncelos <geminivasconceelos@gmail.com>
2023-01-06 22:50:03 -0300

    feat: adicao do post no arquivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import Post from "./components/Post";
 import Sidebar from "./components/Sidebar";
 
 import "./global.css";
@@ -11,7 +12,9 @@ function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main></main>
+        <main>
+          <Post />
+        </main>
       </div>
     </div>
   );
