766b8c74517de03a6b0636c0ee827ecd6ad24436
gevasoncelos <geminivasconceelos@gmail.com>
2023-01-23 21:59:30 -0300

    refactor: adicao do componente avatar no lugar da img

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
@@ -9,20 +10,17 @@ export default function Sidebar() {
         src="https://images.unsplash.com/photo-1617900906639-cab7adceb499?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/geminivasconcelos.png"
-        />
+        <Avatar  src="https://github.com/geminivasconcelos.png" />
 
         <strong>Gemini Vasconcelos</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        
-        <a href="#"><PencilLine  size={20}/> Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} /> Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
 }
- 
